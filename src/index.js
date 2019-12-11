c25f4830d3ed55e119278ebd81777214ec917e7b
Fresco <fresco@frescos-mbp.attlocal.net>
2019-12-11 03:08:21 -0600

    setup cacheExchange, client, and context. notes

--- src/index.js
+++ src/index.js
@@ -4,14 +4,23 @@ import "./styles/index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
-import { Provider, Client, defaultExchanges } from "urql";
+import { Provider, Client, dedupExchange, fetchExchange } from "urql";
+// defaultExchanges array includes: dedupExchange, cacheExchange, fetchExchange
+import { cacheExchange } from "@urql/exchange-graphcache";
+// dedup: if you send same queries at the same time, dedup will make sure only one is sent to API
+// cache: caches operation results (doc cache). caches results from graphQL API by the unique query + variables combination that those results have been requested with
+// fetch: sends graphQL requests using fetch and supports cancellation by default
 
-// gets passed in to Provider as props "value"
+// create new normalized cache
+const cache = cacheExchange({});
+
+// urql needs to tknow the endpoint of the graphQL API to deal with network connections
 const client = new Client({
   url: "http://localhost:4000",
-  exchanges: defaultExchanges
+  exchanges: [dedupExchange, cache, fetchExchange]
 });
 
+// wrap app with context Provider for urql client
 render(
   <Provider value={client}>
     <App />
