743f6d417a91814d827ba84c319d1fa076ab2b87
Fresco <fresco@frescos-mbp.attlocal.net>
2019-12-11 02:51:43 -0600

    configure urql client instance

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import "./styles/index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
-render(<App />, document.getElementById("root"));
+import { Provider, Client, defaultExchanges } from "urql";
+
+// gets passed in to Provider as props "value"
+const client = new Client({
+  url: "http://localhost:4000",
+  exchanges: defaultExchanges
+});
+
+render(
+  <Provider value={client}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
