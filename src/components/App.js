ba715cbb0ea771b266b6b723be8be52b221d522f
Fresco <fresco@frescos-mbp.attlocal.net>
2019-12-12 01:03:12 -0600

    Link components for first GraphQL query via urql

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "../styles/App.css";
 
+import LinkList from "./LinkList";
+
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <LinkList />
+    </div>
+  );
 }
 
 export default App;
