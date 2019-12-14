783738adfa6fa068bd83f8e2734084714cdb91e5
Fresco <fresco@frescos-mbp.attlocal.net>
2019-12-13 18:12:40 -0600

    auth mutations

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { setToken } from "../token";
+
+import gql from "graphql-tag";
+import { useMutation } from "urql";
+
+const SIGNUP_MUTATION = gql`
+  mutation SignupMutation($email: String!, $password: String!, $name: String!) {
+    signup(email: $email, password: $password, name: $name) {
+      token
+    }
+  }
+`;
+
+const LOGIN_MUTATION = gql`
+  mutation LoginMutation($email: String!, $password: String!) {
+    login(emai: $email, password: $password) {
+      token
+    }
+  }
+`;
 
 const Login = props => {
   // Used to switch between login and signup
@@ -8,6 +27,21 @@ const Login = props => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
+
+  const [state, executeMutation] = useMutation(
+    isLogin ? LOGIN_MUTATION : SIGNUP_MUTATION
+  );
+
+  const mutate = useCallback(() => {
+    executeMutation({ email, password, name }).then(({ data }) => {
+      const token = data && data[isLogin ? "login" : "signup"].token;
+
+      if (token) {
+        setToken(token);
+        props.history.push("/");
+      }
+    });
+  }, [executeMutation, props.history, isLogin, email, password, name]);
 
   return (
     <div>
@@ -37,12 +71,19 @@ const Login = props => {
       </div>
 
       <div className="flex mt3">
-        <button type="button" className="pointer mr2 button">
+        <button
+          type="button"
+          className="pointer mr2 button"
+          disabled={state.fetching}
+          onClick={mutate}
+        >
           {isLogin ? "login" : "create account"}
         </button>
+
         <button
           type="button"
           className="pointer button"
+          disabled={state.fetching}
           onClick={() => setIsLogin(!isLogin)}
         >
           {isLogin ? "need to create an account?" : "already have an account?"}
